48bd59ae9162df8594368cd255954fb5c9c9d444
Howard Francis <howardfrancis@upike.edu>
2018-02-08 15:34:39 -0500

    Cleaned WritingImplement.java

--- WritingImplements/src/WritingImplement.java
+++ WritingImplements/src/WritingImplement.java
@@ -5,50 +5,44 @@ abstract class WritingImplement
     private String bodyMaterial;
     private double pointSize;
     private double length; 
-    
-    /*
+
+    /**
      * A constructor for the WritingImplement class
      *
-	  * @param 	bodyMaterial 	The material the object is made of. Must be "plastic", "wooden", or "metal"</param>
-     * @param 	pointsize		The size of the writing point. Must be positive.
-	  */
+     * @param   bodyMaterial    The material the object is made of. Must be "plastic", "wooden", or "metal"</param>
+     * @param   pointsize       The size of the writing point. Must be positive.
+     */
     public WritingImplement(String bodyMaterial, double pointsize)
     {
         setBodyMaterial(bodyMaterial);
         setPointSize(pointsize);
         this.length = 5.5;
     }
-    
-    
+
     /**
-	  * Mutator method for pointSize
-	  *
-	  * @param	pointSize  the size of the writing tip in mm (must be postitive, defaults to 0.5)
-	  */
-	 public void setPointSize(double pointSize)
-   {
-       if (pointSize > 0)
-       {
-           this.pointSize = pointSize;
-       }
-       else    // invalid point size... set to 0
-       {
-           this.pointSize = 0.5;
-       }
+     * Mutator method for pointSize
+     *
+     * @param   pointSize  the size of the writing tip in mm (must be postitive, defaults to 0.5)
+     */
+    public void setPointSize(double pointSize)
+    {
+        if (pointSize > 0) {
+            this.pointSize = pointSize;
+        } else {   // invalid point size... set to 0.5
+            this.pointSize = 0.5;
+        }
     }
 
-	 
     /**
-	  * Accessor method for pointSize
-	  *
-	  * @return the size of the writing tip in mm
-	  */	
+     * Accessor method for pointSize
+     *
+     * @return the size of the writing tip in mm
+     */ 
     public double getPointSize()
     {
-       return pointSize;
+        return pointSize;
     }
-    
-    
+
     /**
      * Method for getting the length
      * 
@@ -56,61 +50,54 @@ abstract class WritingImplement
      */
     public double getLength()
     {
-    	return length;
+        return length;
     }
-    
 
-	 /**
+    /**
      * Accessor method for bodyMaterial      
-	  *
-	  * @return	the material the implement is made of.
-	  */
+     *
+     * @return  the material the implement is made of.
+     */
     public String getBodyMaterial()
     {
         return bodyMaterial;
     }
-	 
-    
-	 /**
+
+    /**
      * Mutator method for bodyMaterial      
-	  *
-	  * @param	bodyMaterial	the material the implment is made of. Must be one of "plastic", "wooden", or "metal".
-	  */
-	 private void setBodyMaterial(String bodyMaterial)
+     *
+     * @param   bodyMaterial    the material the implment is made of. Must be one of "plastic", "wooden", or "metal".
+     */
+    private void setBodyMaterial(String bodyMaterial)
     {
         if ((bodyMaterial == "plastic")
             || (bodyMaterial == "wooden")
-            || (bodyMaterial == "metal"))
-        {
+            || (bodyMaterial == "metal")) {
             this.bodyMaterial = bodyMaterial;
-        }
-        else
-        {
+        } else {
             this.bodyMaterial = "plastic";
         }
-        
+
     }
-        
-       
+
     /**
-	  * A method to simulate the writing implement writing a message.
+     * A method to simulate the writing implement writing a message.
      *
-     * @param	message	the message the writing implement writes
-	  */
+     * @param   message     the message the writing implement writes
+     */
     public void write(String message)
     {
         System.out.println("The " + bodyMaterial + " writing implement just wrote:  " + message);
     }
-    
-	 
-	 /**
-	  * Method to describe the writing implement drawing something
+
+    /**
+     * Method to describe the writing implement drawing something
      *
-     * @param	something	the item being drawn
-	  */
+     * @param   something   the item being drawn
+     */
     public void draw(String something)
     {
         System.out.println("The " + bodyMaterial + " writing implement just drew: " + something);
     }
 
- }
+}
