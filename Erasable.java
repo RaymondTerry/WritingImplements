634d132f44380b21d63df20db3d5cdeed8a0d3fc
RaymondTerry <42840692+RaymondTerry@users.noreply.github.com>
2021-04-26 13:33:47 -0400

    Create Erasable.java

---
+++ Erasable.java
@@ -0,0 +1,16 @@
+
+/**
+ * An Interface used to give erasable properties to all types of utensils beyond just pencils.
+ *
+ * @author Raymond Terry
+ * @version 1
+ */
+public interface Erasable
+{
+    /**
+     * Method to describe the writing implement erasing something.
+     *
+     * @param   something   something being erased.
+     */
+    public void erase(String something);
+}
